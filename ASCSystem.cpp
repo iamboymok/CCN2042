c3e63f4ec70a1dae096aaed78204640e324727a5
iamboy <iamboymok@hotmail.com>
2013-11-25 01:04:13 +0800

    new file:   ASCSystem.cpp 	new file:   GradeScoreMap.txt 	new file:   makefile 	new file:   studentRes.txt

---
+++ ASCSystem.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(int args, char** argv)
+{
+    printf("hi\n");
+
+    printf("\n");
+    
+    return 0;
+}
+
+
---
+++ makefile
@@ -0,0 +1,5 @@
+ASCSystem: ASCSystem.o
+	g++ ASCSystem.o -o ASCSystem
+
+clean:
+	rm ASCSystem *.o
